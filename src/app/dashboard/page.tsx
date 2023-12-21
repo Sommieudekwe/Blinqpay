5fdb6b0c97948371bae0d5d229072449ee9d30b0
Marvelousy20 <afosmarvel@gmail.com>
2023-12-21 15:26:17 +0100

    added routes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,5 @@
 import Layout from "./layout";
 
 export default function Dashboard() {
-  return (
-    <Layout>
-      <div>This is the home page for dashboard</div>
-    </Layout>
-  );
+  return <div></div>;
 }
