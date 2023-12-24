104a520d93e9053fc6d120188f13e05d09a5bb93
Marvelousy20 <afosmarvel@gmail.com>
2023-12-24 14:48:54 +0100

    fixing modal

--- src/components/Dashboard/DashboardMobileTable.tsx
+++ src/components/Dashboard/DashboardMobileTable.tsx
@@ -1,12 +1,28 @@
+"use client";
+
 import { IDashboard } from "@/types";
+import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogOverlay } from "@radix-ui/react-dialog";
+import Image from "next/image";
 
 interface TableProps {
   data: IDashboard[];
 }
 
 export default function MobileTable({ data }: TableProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalType, setModalType] = useState<"pay" | "cancel" | null>(null);
+  // const [selectedId, setSelectedId] = useState<number | null>(null);
+
+  const handleOpenModal = (type: "pay" | "cancel") => {
+    setModalType(type);
+    // setSelectedId(id);
+    setIsOpen(true);
+    console.log(isOpen);
+  };
+
   return (
     <div className="w-full">
       {data.map((d, index) => (
@@ -35,13 +51,36 @@ export default function MobileTable({ data }: TableProps) {
           </div>
 
           <div className="inline-flex flex-col space-y-2">
-            <Button>Pay now</Button>
-            <Button className="text-cancel border border-cancel border-opacity-25">
+            <Button onClick={() => handleOpenModal("pay")}>Pay now</Button>
+            <Button
+              className="text-cancel border border-cancel border-opacity-25"
+              onClick={() => handleOpenModal("cancel")}
+            >
               Cancel
             </Button>
           </div>
         </div>
       ))}
+
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        {/* <DialogOverlay /> */}
+        <DialogContent className="text-center text-white">
+          {modalType === "pay" ? (
+            <div>Successful</div>
+          ) : modalType === "cancel" ? (
+            <div>
+              <Image
+                src="./dashboard/warning.svg"
+                alt="warning"
+                width={88}
+                height={88}
+              />
+
+              <p>Are you sure you want to cancel this order?</p>
+            </div>
+          ) : null}
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
