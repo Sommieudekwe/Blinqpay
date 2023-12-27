d6166f60dd4d3cbc320501809506a972f78cde98
Marvelousy20 <afosmarvel@gmail.com>
2023-12-27 12:06:50 +0100

    made changes

--- src/app/dashboard/bank-history/BankHistoryMobile.tsx
+++ src/app/dashboard/bank-history/BankHistoryMobile.tsx
@@ -21,24 +21,26 @@ export default function BankHistoryDashboard({ data }: TableProps) {
           key={index}
           className="border-b border-t border-white border-opacity-25 py-6 flex justify-between"
         >
-          <div className="space-y-3">
-            {/* <p className="opacity-60">{d.orderNumber}</p> */}
-            <h3 className="opacity-60">{d.accountName}</h3>
-            <p className="opacity-60">{d.accountNumber}</p>
-            <p className="opacity-60">&#8358;{formatAmount(d.amount)}</p>
-            <p className="opacity-60">{d.date}</p>
-            <p
-              className={cn(
-                "capitalize rounded-3xl py-1 text-sm",
-                d.status === "successful"
-                  ? "text-success bg-opacity-10 inline-flex"
-                  : d.status === "failed"
-                  ? "text-failed bg-opacity-10 inline-flex"
-                  : "text-pending bg-opacity-10 inline-flex"
-              )}
-            >
-              {d.status}
-            </p>
+          <div className="flex justify-between w-full">
+            <div className="space-y-3">
+              {/* <p className="opacity-60">{d.orderNumber}</p> */}
+              <h3 className="opacity-60">{d.accountName}</h3>
+              <p className="opacity-60">{d.accountNumber}</p>
+              <p className="opacity-60">&#8358;{formatAmount(d.amount)}</p>
+              <p className="opacity-60">{d.date}</p>
+              <p
+                className={cn(
+                  "capitalize rounded-3xl py-1 text-sm",
+                  d.status === "successful"
+                    ? "text-success bg-opacity-10 inline-flex"
+                    : d.status === "failed"
+                    ? "text-failed bg-opacity-10 inline-flex"
+                    : "text-pending bg-opacity-10 inline-flex"
+                )}
+              >
+                {d.status}
+              </p>
+            </div>
 
             <div className="">
               <Button className="text-right">Get Receipt</Button>
