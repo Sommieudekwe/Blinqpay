412da9ec4c231f1d9b207bf652de318d97221edd
bobbycrooz <idrisloove@gmail.com>
2023-12-28 11:57:39 +0100

    ...

--- src/components/ui/data-table-pagination.tsx
+++ src/components/ui/data-table-pagination.tsx
@@ -28,7 +28,7 @@ export function DataTablePagination<TData> ({ table }: DataTablePaginationProps<
             <SelectValue placeholder={table.getState().pagination.pageSize} />
           </SelectTrigger>
           <SelectContent side="top">
-            {[10, 20, 30, 40, 50].map((pageSize) => (
+            {[5, 6, 7, 8, 10].map((pageSize) => (
               <SelectItem key={pageSize} value={`${pageSize}`}>
                 {pageSize}
               </SelectItem>
