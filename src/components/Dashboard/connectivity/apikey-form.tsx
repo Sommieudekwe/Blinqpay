4bcadea8096b84195fe06d590781a20148bb8359
Marvelousy20 <afosmarvel@gmail.com>
2024-01-24 13:42:13 +0100

    making changes and branching off

--- src/components/Dashboard/connectivity/apikey-form.tsx
+++ src/components/Dashboard/connectivity/apikey-form.tsx
@@ -1,18 +1,16 @@
-
 "use client";
 
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
-import
-{
-      Form,
-      FormControl,
-      FormDescription,
-      FormField,
-      FormItem,
-      FormMessage,
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
 } from "@/components/ui/form";
 import * as yup from "yup";
 import { Input } from "@/components/ui/input";
@@ -24,156 +22,150 @@ import Link from "next/link";
 import { notify } from "@/components/ui/toast";
 
 const ConnectionDetailsSchema = yup.object().shape({
-      api_key: yup.string().required("Api key is required!"),
-      tracking: yup.string().required("Tracking reference is required"),
-      narration: yup.string().required("Naration is required"),
-      agree: yup.boolean().required("Agree to terms"),
-
+  api_key: yup.string().required("Api key is required!"),
+  tracking: yup.string().required("Tracking reference is required"),
+  narration: yup.string().required("Naration is required"),
+  agree: yup.boolean().required("Agree to terms"),
 });
 
 type ConnectionDetailsSchemaTypes = yup.InferType<
-      typeof ConnectionDetailsSchema
+  typeof ConnectionDetailsSchema
 >;
 
-export default function BankAPIDetailsForm({ setStep, handleConnectToBank }: IBankDetailsProps)
-{
-      const { toast } = useToast();
-      const defaultValues: ConnectionDetailsSchemaTypes = {
-            api_key: "",
-            tracking: "",
-            narration: "",
-            agree: false
-      };
+export default function BankAPIDetailsForm({
+  setStep,
+  handleConnectToBank,
+}: IBankDetailsProps) {
+  const { toast } = useToast();
+  const defaultValues: ConnectionDetailsSchemaTypes = {
+    api_key: "",
+    tracking: "",
+    narration: "",
+    agree: false,
+  };
 
-      const form = useForm<ConnectionDetailsSchemaTypes>({
-            resolver: yupResolver(ConnectionDetailsSchema),
-            defaultValues,
-            mode: "all",
-      });
-      /*
-       *
-       *
-       *
-       *
-       */
-      async function onSubmit(values: ConnectionDetailsSchemaTypes)
-      {
-            if (!values.agree)
+  const form = useForm<ConnectionDetailsSchemaTypes>({
+    resolver: yupResolver(ConnectionDetailsSchema),
+    defaultValues,
+    mode: "all",
+  });
+  /*
+   *
+   *
+   *
+   *
+   */
+  async function onSubmit(values: ConnectionDetailsSchemaTypes) {
+    if (!values.agree)
+      return notify.error("Agree to terms and conditions!iiii");
 
-                  return notify.error("Agree to terms and conditions!iiii")
+    handleConnectToBank();
+  }
+  /*
+   *
+   *
+   *
+   *
+   */
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full mt-[3.375rem] "
+      >
+        <div className="space-y-6">
+          <FormField
+            control={form.control}
+            name="api_key"
+            render={({ field }) => (
+              <FormItem>
+                {/* <FormLabel htmlFor="api_key">API Key</FormLabel> */}
+                <FormControl>
+                  <Input
+                    id="api_key"
+                    placeholder="API Key"
+                    {...field}
+                    error={form.formState.errors?.api_key?.message}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Can’t find your API Key?
+                  <Link href={"/"} className="text-button-primary">
+                    Watch this video
+                  </Link>
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            handleConnectToBank();
-      }
-      /*
-       *
-       *
-       *
-       *
-       */
-      return (
+          <FormField
+            control={form.control}
+            name="tracking"
+            render={({ field }) => (
+              <FormItem>
+                {/* <FormLabel htmlFor="secret_key">API Key</FormLabel> */}
+                <FormControl>
+                  <Input
+                    id="tracking"
+                    placeholder="SVirtual Account Tracking Reference"
+                    {...field}
+                    error={form.formState.errors?.tracking?.message}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Don’t have a tracking reference?
+                  <Link href={"/"} className="text-button-primary">
+                    Watch this video
+                  </Link>
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <Form {...form}>
-                  <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="w-full mt-[3.375rem] "
-                  >
-                        <div className="space-y-6">
-                              <FormField
-                                    control={form.control}
-                                    name="api_key"
-                                    render={({ field }) => (
-                                          <FormItem>
-                                                {/* <FormLabel htmlFor="api_key">API Key</FormLabel> */}
-                                                <FormControl>
-                                                      <Input
-                                                            id="api_key"
-                                                            placeholder="API Key"
-                                                            {...field}
-                                                            error={form.formState.errors?.api_key?.message}
-                                                      />
-                                                </FormControl>
-                                                <FormDescription>
-                                                      Can’t find your API Key?
-                                                      <Link href={"/"} className="text-button-primary">
-                                                            Watch this video
-                                                      </Link>
-
-                                                </FormDescription>
-                                                <FormMessage />
-                                          </FormItem>
-                                    )}
-                              />
-
-                              <FormField
-                                    control={form.control}
-                                    name="tracking"
-                                    render={({ field }) => (
-                                          <FormItem>
-                                                {/* <FormLabel htmlFor="secret_key">API Key</FormLabel> */}
-                                                <FormControl>
-                                                      <Input
-                                                            id="tracking"
-                                                            placeholder="SVirtual Account Tracking Reference"
-                                                            {...field}
-                                                            error={form.formState.errors?.tracking?.message}
-                                                      />
-                                                </FormControl>
-                                                <FormDescription>
-                                                      Don’t have a tracking reference?
-                                                      <Link href={"/"} className="text-button-primary">
-                                                            Watch this video
-                                                      </Link>
-                                                </FormDescription>
-                                                <FormMessage />
-                                          </FormItem>
-                                    )}
-                              />
-
-                              <FormField
-                                    control={form.control}
-                                    name="narration"
-                                    render={({ field }) => (
-                                          <FormItem>
-                                                <FormControl>
-                                                      <Input
-                                                            id="narration"
-                                                            placeholder="Transaction Narration"
-                                                            {...field}
-                                                            error={form.formState.errors?.narration?.message}
-                                                      />
-                                                </FormControl>
-                                                <FormMessage />
-                                          </FormItem>
-                                    )}
-                              />
-
-
-                        </div>
-                        <Button variant={"primary"} className="w-full mt-[3.7rem]">
-                              Submit
-                        </Button>
-                        <FormField
-                              control={form.control}
-                              name="agree"
-                              render={({ field }) => (
-                                    <FormItem className="flex space-x-3 space-y-0 mt-6">
-                                          <FormControl>
-                                                <Checkbox
-                                                      checked={field.value}
-                                                      onCheckedChange={field.onChange}
-                                                      className="rounded-full bg-button-connect"
-                                                />
-                                          </FormControl>
-                                          <FormDescription className="mt-0">
-                                                Terms and conditions guiding this information privacy
-                                                policy and all will be here and by clicking submit you
-                                                agree to this privacy policy
-                                          </FormDescription>
-                                    </FormItem>
-                              )}
-                        />
-                  </form>
-            </Form>
-
-      );
+          <FormField
+            control={form.control}
+            name="narration"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    id="narration"
+                    placeholder="Transaction Narration"
+                    {...field}
+                    error={form.formState.errors?.narration?.message}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button variant={"primary"} className="w-full mt-[3.7rem]">
+          Submit
+        </Button>
+        <FormField
+          control={form.control}
+          name="agree"
+          render={({ field }) => (
+            <FormItem className="flex space-x-3 space-y-0 mt-6">
+              <FormControl>
+                <Checkbox
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                  className="rounded-full bg-button-connect"
+                />
+              </FormControl>
+              <FormDescription className="mt-0">
+                Terms and conditions guiding this information privacy policy and
+                all will be here and by clicking submit you agree to this
+                privacy policy
+              </FormDescription>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 }
--- src/components/Dashboard/connectivity/details-form.tsx
+++ src/components/Dashboard/connectivity/details-form.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { Button } from "@/components/ui/button";
@@ -6,14 +5,13 @@ import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
-import
-      {
-            Form,
-            FormControl,
-            FormField,
-            FormItem,
-            FormMessage,
-      } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import * as yup from "yup";
 import { Input } from "@/components/ui/input";
 import React from "react";
@@ -21,125 +19,116 @@ import React from "react";
 import { IBankDetailsProps } from "@/types";
 
 const ConnectionDetailsSchema = yup.object().shape({
-      email: yup.string().required("Email is required!"),
-      accountNumber: yup.string().required("Account is required"),
-      phoneNumber: yup.string().required("Phone number is required"),
+  email: yup.string().required("Email is required!"),
+  accountNumber: yup.string().required("Account is required"),
+  phoneNumber: yup.string().required("Phone number is required"),
 });
 
 type ConnectionDetailsSchemaTypes = yup.InferType<
-      typeof ConnectionDetailsSchema
+  typeof ConnectionDetailsSchema
 >;
 
+export default function BankDetailsForm({
+  setStep,
+}: Pick<IBankDetailsProps, "setStep">) {
+  const defaultValues: ConnectionDetailsSchemaTypes = {
+    email: "",
+    accountNumber: "",
+    phoneNumber: "",
+  };
 
+  const form = useForm<ConnectionDetailsSchemaTypes>({
+    resolver: yupResolver(ConnectionDetailsSchema),
+    defaultValues,
+    mode: "all",
+  });
 
-export default function BankDetailsForm({ setStep }: Pick<IBankDetailsProps, 'setStep'>)
-{
-      const defaultValues: ConnectionDetailsSchemaTypes = {
-            email: "",
-            accountNumber: "",
-            phoneNumber: "",
-      };
+  /*
+   *
+   *
+   *
+   *
+   */
 
-      const form = useForm<ConnectionDetailsSchemaTypes>({
-            resolver: yupResolver(ConnectionDetailsSchema),
-            defaultValues,
-            mode: "all",
-      });
+  async function onSubmit(values: ConnectionDetailsSchemaTypes) {
+    setStep(2);
+  }
+  /*
+   *
+   *
+   *
+   *
+   */
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full mt-[3.375rem] "
+      >
+        <div className="space-y-[1.75rem]">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                {/* <FormLabel htmlFor="api_key">API Key</FormLabel> */}
+                <FormControl>
+                  <Input
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                    {...field}
+                    error={form.formState.errors?.email?.message}
+                  />
+                </FormControl>
 
-      /*
-       *
-       *
-       *
-       *
-       */
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-      async function onSubmit(values: ConnectionDetailsSchemaTypes)
-      {
+          <FormField
+            control={form.control}
+            name="accountNumber"
+            render={({ field }) => (
+              <FormItem>
+                {/* <FormLabel htmlFor="secret_key">API Key</FormLabel> */}
+                <FormControl>
+                  <Input
+                    id="accountNumber"
+                    placeholder="Account Number"
+                    {...field}
+                    error={form.formState.errors?.accountNumber?.message}
+                  />
+                </FormControl>
 
-            setStep(2);
-      }
-      /*
-       *
-       *
-       *
-       *
-       */
-      return (
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <Form {...form}>
-                  <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="w-full mt-[3.375rem] "
-                  >
-                        <div className="space-y-[1.75rem]">
-                              <FormField
-                                    control={form.control}
-                                    name="email"
-                                    render={({ field }) => (
-                                          <FormItem>
-                                                {/* <FormLabel htmlFor="api_key">API Key</FormLabel> */}
-                                                <FormControl>
-                                                      <Input
-                                                            type="email"
-                                                            id="email"
-                                                            placeholder="Email"
-                                                            {...field}
-                                                            error={form.formState.errors?.email?.message}
-                                                      />
-                                                </FormControl>
-
-                                                <FormMessage />
-                                          </FormItem>
-                                    )}
-                              />
-
-                              <FormField
-                                    control={form.control}
-                                    name="accountNumber"
-                                    render={({ field }) => (
-                                          <FormItem>
-                                                {/* <FormLabel htmlFor="secret_key">API Key</FormLabel> */}
-                                                <FormControl>
-                                                      <Input
-                                                            id="accountNumber"
-                                                            placeholder="Account Number"
-                                                            {...field}
-                                                            error={form.formState.errors?.accountNumber?.message}
-                                                      />
-                                                </FormControl>
-
-                                                <FormMessage />
-                                          </FormItem>
-                                    )}
-                              />
-
-                              <FormField
-                                    control={form.control}
-                                    name="phoneNumber"
-                                    render={({ field }) => (
-                                          <FormItem>
-                                                <FormControl>
-                                                      <Input
-                                                            id="phoneNumber"
-                                                            placeholder="Phone Number"
-                                                            {...field}
-                                                            error={form.formState.errors?.phoneNumber?.message}
-                                                      />
-                                                </FormControl>
-                                                <FormMessage />
-                                          </FormItem>
-                                    )}
-                              />
-
-
-
-                        </div>
-                        <Button variant={"primary"} className="w-full mt-[3rem]">
-                              Next
-                        </Button>
-
-                  </form>
-            </Form>
-
-      );
+          <FormField
+            control={form.control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    id="phoneNumber"
+                    placeholder="Phone Number"
+                    {...field}
+                    error={form.formState.errors?.phoneNumber?.message}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button variant={"primary"} className="w-full mt-[3rem]">
+          Next
+        </Button>
+      </form>
+    </Form>
+  );
 }
